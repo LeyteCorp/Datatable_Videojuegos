050aa64989d247bf5f4ebf48430ea7e85f0c9800
LeyteCorp <72232765+LeyteCorp@users.noreply.github.com>
2020-11-26 21:24:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   <link rel="stylesheet" href="estilos/estilos.css">
+    <meta charset="UTF-8">
+    <title>Start</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body style="background-image: url(img/fondo2.jpg)">
+    <a href="inicio.php" class="btn-neon">
+        <span id="span1"></span>
+        <span id="span2"></span>
+        <span id="span3"></span>
+        <span id="span4"></span>
+         Start
+    </a>
+</body>
+</html>
